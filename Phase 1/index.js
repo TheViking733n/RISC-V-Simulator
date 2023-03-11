d2228e9008e97e3b3cdf22ccf03cd1ab66401316
TheViking733n <anant733n@gmail.com>
2023-03-12 04:44:02 +0530

    created class template

--- Phase 1/index.js
+++ Phase 1/index.js
@@ -1,6 +1,35 @@
 class Simulator {
     constructor() {
-        this.cycle = 0;
+        this.CYCLE = 0;  // Cycle number
+        this.PC = 0;     // Program counter
+        this.INSTRUCTION = "";   // Stores current instruction from Fetch
+        this.RF = new Array(32);   // Register file
+        this.RF.fill(0);
+        this.OP = "";   // Stores operator decoded from Decode
+        this.RS1 = 0;  // 0 to 31 for each register
+        this.RS2 = 0;  // 0 to 31 for each register
+        this.RD = 0;  // 0 to 31 for each register
+        this.IMM = 0; // Stores immediate value
+        this.ALURESULT = 0; // Stores result of ALU
     }
-    
+
+    fetch() {
+
+    }
+
+    decode() {
+
+    }
+
+    execute() {
+
+    }
+
+    memoryAccess() {
+
+    }
+
+    writeBack() {
+
+    }
 }
