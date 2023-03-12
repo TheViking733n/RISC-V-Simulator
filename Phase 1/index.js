426079e3723372940953adb59230574eb01a3e48
TheViking733n <anant733n@gmail.com>
2023-03-12 09:07:58 +0530

    created execute function

--- Phase 1/index.js
+++ Phase 1/index.js
@@ -1,4 +1,31 @@
+function timelineAppendRow(cycle, row) {
+    // cycle is value of current cycle number
+    // row is an array of size 5 containing 5 numbers which represent the instruction number being executed
+    // row value 0 means stall
+    cycleDiv = document.createElement("div");
+    cycleDiv.className = "timeline-cell";
+    cycleDiv.innerHTML = cycle;
+    document.getElementById("cycle").appendChild(cycleDiv);
 
+    let timelineHeaderId = ["fetch", "decode", "execute", "memoryaccess", "writeback"];
+    for (let i = 0; i < row.length; i++) {
+        let instructionDiv = document.createElement("div");
+        instructionDiv.className = "timeline-cell";
+        if (row[i] != 0) {
+            instructionDiv.innerHTML = row[i];
+        } else {
+            // let stallDiv = document.createElement("div");
+            // stallDiv.className = "stall";
+            // instructionDiv.appendChild(stallDiv);
+            instructionDiv.innerHTML = "O";
+
+        }
+        document.getElementById(timelineHeaderId[i]).appendChild(instructionDiv);
+    }
+}
+for (let i = 1; i < 51; i++) {
+    timelineAppendRow(i, [i, i, (i%7!=0)+i, (i%7==0)+i, i&1]);
+}
 
 class Simulator {
     constructor() {
@@ -26,7 +53,119 @@ class Simulator {
     }
 
     execute() {
-
+        let rs1 = Math.round(this.RF[Math.round(this.RS1)]);
+        let rs2 = Math.round(this.RF[Math.round(this.RS2)]);
+        let imm = Math.round(this.IMM);
+        switch (this.OP) {
+          case "add":
+            this.ALURESULT = rs1 + rs2;
+            break;
+          case "sub":
+            this.ALURESULT = rs1 - rs2;
+            break;
+          case "xor":
+            this.ALURESULT = rs1 ^ rs2;
+            break;
+          case "or":
+            this.ALURESULT = rs1 | rs2;
+            break;
+          case "and":
+            this.ALURESULT = rs1 & rs2;
+            break;
+          case "sll":
+            this.ALURESULT = rs1 << rs2;
+            break;
+          case "srl":
+            this.ALURESULT = rs1 >>> rs2;
+            break;
+          case "sra":
+            this.ALURESULT = rs1 >> rs2;
+            break;
+          case "slt":
+            this.ALURESULT = rs1 < rs2 ? 1 : 0;
+            break;
+          case "sltu":
+            this.ALURESULT = Math.abs(rs1) < Math.abs(rs2) ? 1 : 0;
+            break;
+          case "addi":
+            this.ALURESULT = rs1 + imm;
+            break;
+          case "xori":
+            this.ALURESULT = rs1 ^ imm;
+            break;
+          case "ori":
+            this.ALURESULT = rs1 | imm;
+            break;
+          case "andi":
+            this.ALURESULT = rs1 & imm;
+            break;
+          case "slli":
+            this.ALURESULT = rs1 << imm;
+            break;
+          case "srli":
+            this.ALURESULT = rs1 >> imm;
+            break;
+          case "srai":
+            this.ALURESULT = rs1 >> imm;
+            break;
+          case "slti":
+            this.ALURESULT = rs1 < imm ? 1 : 0;
+            break;
+          case "sltiu":
+            this.ALURESULT = Math.abs(rs1) < Math.abs(imm) ? 1 : 0;
+            break;
+          case "lb":
+          case "lh":
+          case "lw":
+          case "lbu":
+          case "lhu":
+          case "sb":
+          case "sh":
+          case "sw":
+            this.ALURESULT = rs1 + imm;
+            break;
+          case "beq":
+            if (rs1 == rs2) this.PC += (imm >> 2);
+            else this.PC += 1;
+            break;
+          case "bne":
+            if (rs1 != rs2) this.PC += (imm >> 2);
+            else this.PC += 1;
+            break;
+          case "blt":
+            if (rs1 < rs2) this.PC += (imm >> 2);
+            else this.PC += 1;
+            break;
+          case "bge":
+            if (rs1 >= rs2) this.PC += (imm >> 2);
+            else this.PC += 1;
+            break;
+          case "bltu":
+            if (Math.abs(rs1) < Math.abs(rs2)) this.PC += (imm >> 2);
+            else this.PC += 1;
+            break;
+          case "bgeu":
+            if (Math.abs(rs1) >= Math.abs(rs2)) this.PC += (imm >> 2);
+            break;
+          case "jal":
+            this.ALURESULT = 4 * (this.PC + 1);
+            this.PC += (imm >> 2);
+            break;
+          case "jalr":
+            this.ALURESULT = 4 * (this.PC + 1);
+            this.PC = (rs1 >> 2) + (imm >> 2);
+            break;
+          case "lui":
+            this.ALURESULT = imm << 12;
+            break;
+          case "auipc":
+            this.ALURESULT = 4*this.PC + (imm << 12);
+            break;
+            default:
+                console.error("Error");
+                break;
+        }
+    
     }
 
     memoryAccess() {
