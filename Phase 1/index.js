b9fc089e78434610df2a1c610926e7d7d231f97a
TheViking733n <anant733n@gmail.com>
2023-03-12 05:56:40 +0530

    update

--- Phase 1/index.js
+++ Phase 1/index.js
@@ -1,15 +1,17 @@
+
+
 class Simulator {
     constructor() {
-        this.CYCLE = 0;  // Cycle number
-        this.PC = 0;     // Program counter
-        this.INSTRUCTION = "";   // Stores current instruction from Fetch
+        this.CYCLE = 0;   // Cycle number
+        this.PC = 0;      // Program counter
+        this.INSTRUCTION = "";     // Stores current instruction from Fetch
         this.RF = new Array(32);   // Register file
         this.RF.fill(0);
-        this.OP = "";   // Stores operator decoded from Decode
-        this.RS1 = 0;  // 0 to 31 for each register
-        this.RS2 = 0;  // 0 to 31 for each register
-        this.RD = 0;  // 0 to 31 for each register
-        this.IMM = 0; // Stores immediate value
+        this.OP = "";       // Stores operator decoded from Decode
+        this.RS1 = 0;       // 0 to 31 for each register
+        this.RS2 = 0;       // 0 to 31 for each register
+        this.RD = 0;        // 0 to 31 for each register
+        this.IMM = 0;       // Stores immediate value
         this.ALURESULT = 0; // Stores result of ALU
     }
 
